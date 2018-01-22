61e59379d4164368fa31f48d9c104826cd2d02ce
Kaitlyn Folsom <kaitlynmfolsom@gmail.com>
2018-01-22 15:08:31 -0500

    update db connection

--- includes/db.php
+++ includes/db.php
@@ -1,6 +1,18 @@
 <?php 
 
-$connection = mysqli_connect('localhost', 'root', 'root', 'cms_blog');
+//Create variables for our sql connection
+$db['db_host'] = "localhost";
+$db['db_user'] = "root";
+$db['db_password'] = "root";
+$db['db_name'] = "cms_blog";
+
+// We loop through each key in our $db variable and create a constant
+foreach($db as $key => $value) {
+	define(strtoupper($key), $value);
+}
+
+// inter our constant variables into our connection function
+$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
 if($connection) {
 	echo "We are connected";
