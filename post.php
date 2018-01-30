49501dfe0d3493973dba4bff3bb8f0094d935a94
Kaitlyn Folsom <kaitlynmfolsom@gmail.com>
2018-01-29 21:13:58 -0500

    increment comment count

--- post.php
+++ post.php
@@ -58,6 +58,9 @@
 						if(!$create_comment_query) {
 							die("Query failed" . mysqli_error($connection));
 						}
+						
+						$query = "UPDATE posts SET post_comment_count = post_comment_count + 1 WHERE post_id = $post_id ";
+						$update_comment_count = mysqli_query($connection, $query);
 					}
 					?>
 
