165a29ef6da22ca8635c9682897a77ece93ba1e6
Kaitlyn Folsom <kaitlynmfolsom@gmail.com>
2018-01-29 21:17:24 -0500

    fix refresh when deleting a post

--- admin/components/view_all_posts.php
+++ admin/components/view_all_posts.php
@@ -68,7 +68,8 @@
 		$the_post_id = $_GET['delete'];
 		
 		$query = "DELETE FROM posts WHERE post_id = {$the_post_id} ";
-		
 		$delete_query = mysqli_query($connection, $query);
+		
+		header("location: posts.php");
 	}
 ?>
